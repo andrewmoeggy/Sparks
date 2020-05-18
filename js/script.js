c88a5d2f0a2c836244889eb6e021ca1c8ad03abd
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-05-18 18:05:33 -0400

    m

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 
-console.log(spark)
-
-function myFunction() {
-}
-
 function sparkPick() {
   const spark = document.getElementById('spark');
   spark.classList = 'main-heading';
   const animation = document.getElementById('animation');
   spark.classList += ` ${animation.options[animation.selectedIndex].text}`
-  console.log(document.getElementById('spark').value)
+  console.log(document.getElementById('spark'))
 }
